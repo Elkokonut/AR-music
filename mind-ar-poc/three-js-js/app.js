45fc514db869d6882ea849d0ebb2f83b5a809921
Marine Charra <marine.charra35@gmail.com>
2022-03-09 11:20:29 +0100

    [POC 6] Update for parcel

--- mind-ar-poc/three-js-js/app.js
+++ mind-ar-poc/three-js-js/app.js
@@ -1,25 +1,26 @@
 import 'mind-ar/dist/mindar-face-three.prod';
 
 /*eslint no-undef: 0*/
-window.addEventListener("DOMContentLoaded", async function () {
-    const THREE = MINDAR.FACE.THREE;
-    const mindarThree = new MINDAR.FACE.MindARThree({
-        container: document.querySelector("#container"),
-    });
-    const { renderer, scene, camera } = mindarThree;
-    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-    scene.add(light);
-    const faceMesh = mindarThree.addFaceMesh();
-    const texture = new THREE.TextureLoader().load('../assets/canonical_face_model_uv_visualization.png');
-    faceMesh.material.map = texture;
-    faceMesh.material.transparent = true;
-    faceMesh.material.needsUpdate = true;
-    scene.add(faceMesh);
-    const start = async () => {
-        await mindarThree.start();
-        renderer.setAnimationLoop(() => {
-            renderer.render(scene, camera);
-        });
-    }
-    start();
+const THREE = window.MINDAR.FACE.THREE;
+const mindarThree = new window.MINDAR.FACE.MindARThree({
+    container: document.querySelector("#container"),
 });
+
+var link = require('../assets/canonical_face_model_uv_visualization.png')
+
+const { renderer, scene, camera } = mindarThree;
+const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+scene.add(light);
+const faceMesh = mindarThree.addFaceMesh();
+const texture = new THREE.TextureLoader().load(link);
+faceMesh.material.map = texture;
+faceMesh.material.transparent = true;
+faceMesh.material.needsUpdate = true;
+scene.add(faceMesh);
+const start = async () => {
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+        renderer.render(scene, camera);
+    });
+}
+start();
