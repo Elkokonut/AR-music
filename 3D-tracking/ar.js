69e05105e5ac7571cf96f0645410582c5c7e85c3
Marine Charra <marine.charra35@gmail.com>
2022-03-28 17:36:07 +0200

    [POC9] Getting a distance value

--- 3D-tracking/ar.js
+++ 3D-tracking/ar.js
@@ -11,7 +11,6 @@ class Object3D {
         this.scene = scene;
         this.obj = obj;
         this.obj.name = name;
-        this.type = "object3D";
         if (position)
             this.obj.position.set(position[0], position[1], position[2]);
         if (scale)
@@ -22,12 +21,12 @@ class Object3D {
 
 
 class BodyTrackerObject extends Object3D {
-    constructor(obj, name, scene, position, scale, keypoint_name) {
+    constructor(obj, name, type, scene, position, scale, keypoint_name) {
         super(obj, name, scene, position, scale);
         this.obj.visible = false;
+        this.type = type;
         this.keypoint_name = keypoint_name;
         this.euroFilter = null;
-        this.type = "BodyTrackerObject";
     }
 
     animate(mesh, width, height) {
@@ -60,8 +59,6 @@ class BodyTrackerObject extends Object3D {
             // console.log(positions);
             this.obj.position.x = positions[0];
             this.obj.position.y = positions[1];
-            if ((this.obj.name == "nose"))
-                console.log(this.obj.position);
         }
         else
             this.obj.visible = false;
@@ -70,15 +67,15 @@ class BodyTrackerObject extends Object3D {
 
 
 class Cube extends BodyTrackerObject {
-    constructor(name, scale, color, keypoint_name, scene) {
+    constructor(name, type, scale, color, keypoint_name, scene) {
         const geometry = new THREE.BoxGeometry();
         const material = new THREE.MeshBasicMaterial({ color: color });
         var cube = new THREE.Mesh(geometry, material);
-        super(cube, name, scene, null, scale, keypoint_name);
+        super(cube, name, type, scene, null, scale, keypoint_name);
         this.cube = cube;
-        this.type = "cube";
+        this.scale = scale;
     }
-    animate(mesh, width, height) {
+    animate(mesh, distance, width, height) {
         this.cube.rotation.x += 0.01;
         this.cube.rotation.y += 0.01;
         super.animate(mesh, width, height)
@@ -86,16 +83,19 @@ class Cube extends BodyTrackerObject {
 }
 
 class Disk extends BodyTrackerObject {
-    constructor(name, color, scene) {
+    constructor(name, type, color, scene) {
         const material = new THREE.MeshBasicMaterial({ color: color });
         const geometry = new THREE.CircleGeometry(5, 32);
         var circle = new THREE.Mesh(geometry, material);
-        super(circle, name, scene, null, null, name);
+        super(circle, name, type, scene, null, null, name);
         this.circle = circle;
-        this.type = "Disk";
     }
-    animate(mesh, width, height) {
-        super.animate(mesh, width, height)
+    animate(mesh, distance, width, height) {
+        super.animate(mesh, width, height);
+        if (distance[this.type])
+            this.obj.scale.x = (distance[this.type] + 1);
+            this.obj.scale.y = (distance[this.type] + 1);
+            this.obj.scale.z = (distance[this.type] + 1);
     }
 }
 
@@ -133,25 +133,21 @@ class Scene {
 
         this.renderer.setSize(this.width, this.height);
         document.body.appendChild(this.renderer.domElement);
-        // document.querySelector('canvas').style = '-moz-transform: scale(-1, 1); \
-        //                                             -webkit-transform: scale(-1, 1); \
-        //                                             -o-transform: scale(-1, 1); \
-        //                                             transform: scale(-1, 1); \
-        //                                             filter: FlipH;\
-        //                                             width: 100%; \
-        //                                             max-width: 100%; \
-        //                                             height: 100%;';
-
-
-
-
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-
-        this.addGridHelper();
+        document.querySelector('canvas').style = '-moz-transform: scale(-1, 1); \
+                                                    -webkit-transform: scale(-1, 1); \
+                                                    -o-transform: scale(-1, 1); \
+                                                    transform: scale(-1, 1); \
+                                                    filter: FlipH;\
+                                                    width: 100%; \
+                                                    max-width: 100%; \
+                                                    height: 100%;';
 
         this.initialisation = true;
     }
 
+    addControls() {
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    }
 
     addGridHelper() {
         const gridHelper = new THREE.GridHelper(1000, 100, 0xff0000);
@@ -175,45 +171,99 @@ class Scene {
 
 export default class BodyTrackerScene extends Scene {
 
-    #keypoints_names = [
-        "nose",
-        "left_eye_inner",
-        "left",
-        "left_eye_outer",
-        "right_eye_inner",
-        "right_eye",
-        "right_eye_outer",
-        "left_ear",
-        "right_ear",
-        "mouth_left",
-        "mouth_right",
-        "left_shoulder",
-        "right_shoulder",
-        "left_elbow",
-        "right_elbow",
-        "left_wrist",
-        "right_wrist",
-        "left_pinky",
-        "right_pinky",
-        "left_index",
-        "right_index",
-        "left_thumb",
-        "right_thumb",
-        "left_hip",
-        "right_hip",
-        "left_knee",
-        "right_knee",
-        "left_ankle",
-        "right_ankle",
-        "left_heel",
-        "right_heel",
-        "left_foot_index",
-        "right_foot_index"
-    ]
+    keypoints_infos = {
+        "face": [
+            [
+                "nose",
+                "left_eye_inner",
+                "left",
+                "right",
+                "left_eye_outer",
+                "right_eye_inner",
+                "right_eye",
+                "right_eye_outer",
+                "left_ear",
+                "right_ear",
+                "mouth_left",
+                "mouth_right"
+            ],
+            [
+                "left",
+                "right"
+            ],
+            0xffff00,
+            {
+                "min": "10",
+                "max": "20"
+            }
+        ],
+        "body": [
+            [
+                "left_shoulder",
+                "right_shoulder",
+                "left_elbow",
+                "right_elbow",
+                "left_hip",
+                "right_hip",
+                "left_knee",
+                "right_knee",
+                "left_ankle",
+                "right_ankle",
+                "left_heel",
+                "right_heel",
+                "left_foot_index",
+                "right_foot_index"
+            ],
+            [
+                "left_shoulder",
+                "right_shoulder"
+            ],
+            0xff00ff,
+            {
+                "min": "100",
+                "max": "300"
+            }
+        ],
+        "right_hand": [
+            [
+                "right_wrist",
+                "right_pinky",
+                "right_index",
+                "right_thumb"
+            ],
+            [
+                "right_wrist",
+                "right_thumb"
+            ],
+            0x00FFAC,
+            {
+                "min": "10",
+                "max": "50"
+            }
+        ],
+        "left_hand": [
+            [
+                "left_wrist",
+                "left_pinky",
+                "left_index",
+                "left_thumb"
+            ],
+            [
+                "left_wrist",
+                "left_thumb"
+            ],
+            0x00F9FF,
+            {
+                "min": "10",
+                "max": "50"
+            }
+        ]
+    }
 
     constructor(video) {
         super(video);
         this.pose_detector = new poseDetector(video);
+        this.distances = {};
     }
 
     async init(width = null, height = null) {
@@ -221,15 +271,34 @@ export default class BodyTrackerScene extends Scene {
         await this.pose_detector.init();
         console.log(`Loaded model:${this.pose_detector.detectorConfig.modelType}`)
 
-        this.objects.push(new Cube("leftHandCube", [20, 20, 20], 0x00ff00, "left_wrist", this.scene));
-        this.objects.push(new Cube("rightHandCube", [20, 20, 20], 0x0000ff, "right_wrist", this.scene));
+        this.objects.push(new Cube("leftHandCube", "left_hand", [20, 20, 20], 0x00ff00, "left_wrist", this.scene));
+        this.objects.push(new Cube("rightHandCube", "right_hand", [20, 20, 20], 0x0000ff, "right_wrist", this.scene));
 
-        for (var index in this.#keypoints_names) {
-            if (this.#keypoints_names[index].includes("left"))
-                this.objects.push(new Disk(this.#keypoints_names[index], 0xffff00, this.scene));
-            else
-                this.objects.push(new Disk(this.#keypoints_names[index], 0xff00ff, this.scene));
+        for (const [type, keypoints_info] of Object.entries(this.keypoints_infos)) {
+            this.distances[type] = 0;
+            var keypoints_names = keypoints_info[0];
+            var color = keypoints_info[2];
+            for (var index in keypoints_names)
+                this.objects.push(new Disk(keypoints_names[index], type, color, this.scene));
         }
+    }
+
+    editDistances(tmp_positions) {
+        for (const [type, dist] of Object.entries(this.distances)) {
+            if (tmp_positions[type] && tmp_positions[type].length == 2) {
+                var min = this.keypoints_infos[type][3]["min"];
+                var max = this.keypoints_infos[type][3]["max"];
+                var mean = (max + min) / 2;
+                var std = (max + min);
+                var tmp = distance(tmp_positions[type][0], tmp_positions[type][1]);
+                tmp = Math.min(max, tmp);
+                tmp = Math.max(min, tmp);
+                tmp = (tmp - mean) / std;
+                this.distances[type] = tmp;
+            }
+        }
+        console.log("distances: ");
+        console.log(this.distances);
     }
 
 
@@ -247,11 +316,20 @@ export default class BodyTrackerScene extends Scene {
         async function render() {
             var mesh = await self.pose_detector.predictFrameKeypoints2d();
             nb_calls_render++;
+            var distances = {};
             self.objects.forEach(obj => {
-                obj.animate(mesh, self.width, self.height);
+                obj.animate(mesh, self.distances, self.width, self.height);
+                if (obj.constructor.name == "BodyTrackerObject" || Object.getPrototypeOf(obj.constructor).name == "BodyTrackerObject") {
+                    var infos = self.keypoints_infos[obj.type];
+                    if (infos && infos[1] && infos[1].includes(obj.obj.name)) {
+                        if (!distances[obj.type])
+                            distances[obj.type] = [];
+                        distances[obj.type].push([obj.obj.position.x, obj.obj.position.y]);
+                    }
+                }
             });
 
-            self.controls.update();
+            self.editDistances(distances);
 
             self.renderer.render(self.scene, self.camera);
 
